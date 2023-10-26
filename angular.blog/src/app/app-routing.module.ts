6d87e284a48bca238d81153b344155bd49312658
Simon-Eriksson <127835440+Simon-Eriksson@users.noreply.github.com>
2023-10-26 18:20:57 +0200

    component, service and routing

--- angular.blog/src/app/app-routing.module.ts
+++ angular.blog/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { BlogPageComponent } from './blog-page/blog-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { AdminComponent } from './admin/admin.component';
+import { CreatePostComponent } from './create-post/create-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'blog/:id', component: BlogPageComponent },
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'admin/create-post', component: CreatePostComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular.blog/src/app/app.module.ts
+++ angular.blog/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { BlogPageComponent } from './blog-page/blog-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { AdminComponent } from './admin/admin.component';
+import { CreatePostComponent } from './create-post/create-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    MainPageComponent,
+    BlogPageComponent,
+    AboutMeComponent,
+    AdminComponent,
+    CreatePostComponent
   ],
   imports: [
     BrowserModule,
